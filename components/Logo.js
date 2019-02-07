3edd5babd56389346aadf50fd7dbb3f0511592fb
capJavert <ante.baric3@gmail.com>
2019-02-07 20:39:05 +0100

    update(app): logo adjustment for android navigation bar

--- components/Logo.js
+++ components/Logo.js
@@ -1,6 +1,12 @@
 import React from 'react'
-import { Image } from 'react-native'
+import { Image, Platform, StyleSheet } from 'react-native'
 
-const Logo = () => <Image source={require('../assets/images/logo.png')} />
+const styles = StyleSheet.create({
+    logo: {
+        marginLeft: Platform.OS === 'android' ? 20 : 0
+    }
+})
+
+const Logo = () => <Image style={styles.logo} source={require('../assets/images/logo.png')} />
 
 export default Logo
